96e308ad6bfa89b5046396be70818c1ca0987256
welysonF5 <Davyd.wa.oliveira@gmail.com>
2025-01-27 20:21:19 -0300

    Gambiarra

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Teste de Construção de Interface!
+          Teste de Construção de Interface funciona!
         </p>
         <a
           className="App-link"
